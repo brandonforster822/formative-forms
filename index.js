de44d55c22b46d22fa602fcb4b3f13459a0ce9c4
Brandon Forster <brandonforster@live.com>
2020-12-16 14:38:09 -0700

    Working on phase 2

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require("express");
+// const cookieParser = require("cookie-parser")
+// const cSurf = require("csurf")
+
+// const cSurfProtection = cSurf({cookie: true})
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,6 +23,10 @@ app.get("/", (req, res) => {
   // res.send("Hello World!");
   res.render('index', { users })
 });
+
+// app.get("/create", cSurfProtection, async(req, res) => {
+
+// })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
